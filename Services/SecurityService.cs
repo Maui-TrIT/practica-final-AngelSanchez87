afbefb73b0023d07ae85813c9d5b3f2fda66974d
Angel Sanchez <angel.sanchez@sedecal.com>
2023-10-31 17:03:08 +0100

    Refactoring

--- Services/SecurityService.cs
+++ Services/SecurityService.cs
@@ -21,26 +21,9 @@ public class SecurityService
     public async Task<bool> Login(string email, string password)
     {
         var url = $"{_settings.UrlBase}/api/usuario/login";
-        var loginRequest = new LoginRequest { Email = email, Password = password };
+        var request = new LoginRequest { Email = email, Password = password };
 
-        var json = JsonConvert.SerializeObject(loginRequest);
-        var content = new StringContent(json, Encoding.UTF8, "application/json");
-        var response = await _client.PostAsync(url, content);
-
-        if (!response.IsSuccessStatusCode)
-            return false;
-
-        var jsonResult = await response.Content.ReadAsStringAsync();
-        var result = JsonConvert.DeserializeObject<UsuarioResponse>(jsonResult);
-
-        Preferences.Set("accesstoken", result.Token);
-        Preferences.Set("userid", result.Id);
-        Preferences.Set("email", result.Email);
-        Preferences.Set("nombre", $"{result.Nombre} {result.Apellido}");
-        Preferences.Set("telefono", result.Telefono);
-        Preferences.Set("username", result.UserName);
-
-        return true;
+        return await SecurityUserRequest(request, url);
     }
 
     public async Task<bool> RegisterUsuario(
@@ -52,7 +35,7 @@ public class SecurityService
         string password)
     {
         var url = $"{_settings.UrlBase}/api/usuario/registrar";
-        var loginRequest = new UsuarioRegisterRequest
+        var request = new UsuarioRegisterRequest
         {
             Nombre = nombre,
             Apellido = apellido,
@@ -62,7 +45,12 @@ public class SecurityService
             Password = password 
         };
 
-        var json = JsonConvert.SerializeObject(loginRequest);
+        return await SecurityUserRequest(request, url);
+    }
+
+    private async Task<bool> SecurityUserRequest(object request, string url)
+    {
+        var json = JsonConvert.SerializeObject(request);
         var content = new StringContent(json, Encoding.UTF8, "application/json");
         var response = await _client.PostAsync(url, content);
 
